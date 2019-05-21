4636431eed30bf21db7de8d62dd70ab4e05e9054
swang7 <swang7@washington.edu>
2019-05-20 19:13:29 -0700

    commit

---
+++ app/src/main/java/com/example/temperatureconverter/ConverterClass.java
@@ -0,0 +1,10 @@
+package com.example.temperatureconverter;
+
+public class ConverterClass {
+    public double c(double fahrenheit){
+
+        return (fahrenheit-32)/1.8;
+
+
+    }
+}
---
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.java
@@ -0,0 +1,153 @@
+/*
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.text.NumberFormat;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+public class MainActivity extends AppCompatActivity {
+
+    // define variables for the widgets
+    private EditText fahrenheitTemperature;
+    private TextView c;
+    private TextView f;
+    private TextView display;
+
+
+    // define the SharedPreferences object
+    //private SharedPreferences savedValues;
+
+    // define instance variables that should be saved
+    //private String fahrenheitString = "";
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            // get references to the widgets
+            c = findViewById(R.id.c);
+            f = findViewById(R.id.f);
+            display = findViewById(R.id.display);
+            fahrenheitTemperature = findViewById(R.id.fahrenheitTemperature);
+
+            userInput();
+
+            // set the listeners
+            //fahrenheitTemperature.setOnEditorActionListener(this);
+
+            // get SharedPreferences object
+            //savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
+        }
+
+
+        private void userInput(){
+
+            c.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
+                    ConverterClass converterClass = new ConverterClass();
+                    double degCel = Double.parseDouble(fahrenheitTemperature.getText().toString());
+                    display.setText(Double.toString(degCel));
+                }
+            });
+        }
+
+    }
+
+
+}
+*/
+
+package com.example.temperatureconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.text.NumberFormat;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText fahrenheitTemperature;
+    private TextView c;
+    private TextView f;
+    private TextView Display;
+    private TextView output_TV;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        c = findViewById(R.id.c);
+        f = findViewById(R.id.f);
+        Display = findViewById(R.id.Display);
+        fahrenheitTemperature = findViewById(R.id.fahrenheitTemperature);
+        //output_TV = findViewById(R.id.output_tv);
+
+        //userInput();
+
+        //}
+
+        //private void userInput(){
+
+        //c.setOnClickListener(new View.OnClickListener() {
+
+        // public void onClick(View v) {
+        ConverterClass converterClass = new ConverterClass();
+        double degCel = Double.parseDouble(fahrenheitTemperature.getText().toString());
+        double degFar = converterClass.c(degCel);
+        //Display.setText(Double.toString(degCel));
+        //output_TV.setText(Double.toString(degFar));
+
+
+    }
+
+}
+        //});
+
+        /*
+        f2c_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConverterClass converterClass=new ConverterClass();
+                double degFar=Double.parseDouble(input_Edt.getText().toString());
+                double degCel=converterClass.f2c_fn(degFar);
+                output_TV.setText(Double.toString(degCel));
+
+            }
+        });
+        */
+
+
